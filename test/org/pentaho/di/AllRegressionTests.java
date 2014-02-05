90eef1f285a1be095ab9ac0c31590527b2e3799c
Dzmitry <dima.prokopenko@gmail.com>
2014-02-05 15:00:31 +0300

    PDI-11373 - Database Join step fails with Number of rows to return on MySQL
    junit tests added

--- test/org/pentaho/di/AllRegressionTests.java
+++ test/org/pentaho/di/AllRegressionTests.java
@@ -75,7 +75,7 @@ public class AllRegressionTests {
     suite.addTestSuite( StringEvaluatorTest.class );
     suite.addTestSuite( ParameterSimpleTransTest.class );
     suite.addTestSuite( ValueDataUtilTest.class );
-    suite.addTestSuite( DatabaseTest.class );
+    suite.addTest( new JUnit4TestAdapter( DatabaseTest.class ) );
     suite.addTestSuite( HopTest.class );
     suite.addTestSuite( InjectorTest.class );
     suite.addTestSuite( RowGeneratorTest.class );
@@ -101,7 +101,7 @@ public class AllRegressionTests {
     suite.addTest( new JUnit4TestAdapter( GPLoadTest.class ) );
     suite.addTest( new JUnit4TestAdapter( TipsDialogTest.class ) );
     suite.addTest( new JUnit4TestAdapter( TransExecutorTest.class ) );
-    
+
     // Temporarily disable this test, it never worked on Windows or Unix so
     // it doesn't make sense executing it for the moment.
     // suite.addTestSuite( BlackBoxTests.class );
